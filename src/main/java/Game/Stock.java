fa071b42c83c798af267e3b7fad41eaf5c6a2644
mariodiodoardo <37807412+mariodiodoardo@users.noreply.github.com>
2018-05-04 15:37:02 +0200

    classe stock

--- src/main/java/Game/Stock.java
+++ src/main/java/Game/Stock.java
@@ -4,6 +4,35 @@ import Dice.Die;
 
 import java.util.ArrayList;
 
+import static java.lang.System.out;
+
 public class Stock {
-    private ArrayList<Die> dicefund;
+    private ArrayList<Die> dicestock;
+
+    public Stock(int n) {
+        dicestock=new ArrayList<Die>(2*n+1);
+    }
+
+    public void reset_stock(){
+        dicestock.clear();
+    }
+
+    public void show_riserva(){
+        for (Die d : dicestock){
+            out.println("index dado:"+d.toString());
+        }
+    }
+
+    public void addDie(Die d){
+        dicestock.add(d);
+    }
+
+    public Die extract_die(int index){
+        Die temp=new Die();
+        temp = dicestock.get(index);
+        dicestock.remove(index);
+        out.println("new Stock:");
+        show_riserva();
+        return temp;
+    }
 }
