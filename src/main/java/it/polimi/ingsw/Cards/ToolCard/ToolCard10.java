7546596ca5a340ad1de184270d46586625032479
demon <sara.demonaco@mail.polimi.it>
2018-05-13 12:09:21 +0200

    Carta Tool 10

--- src/main/java/it/polimi/ingsw/Cards/ToolCard/ToolCard10.java
+++ src/main/java/it/polimi/ingsw/Cards/ToolCard/ToolCard10.java
@@ -1,6 +1,8 @@
 package it.polimi.ingsw.Cards.ToolCard;
 
 import it.polimi.ingsw.Cards.Tool;
+import it.polimi.ingsw.Dice.Colour;
+import it.polimi.ingsw.Dice.Die;
 
 public class ToolCard10 extends Tool {
     public ToolCard10() {
@@ -8,6 +10,19 @@ public class ToolCard10 extends Tool {
                 "giralo sulla faccia opposta\n\n" +
                 "6 diventa 1, 5 diventa 2, 4 diventa 3 ecc.");
         super.setName("Tampone Diamantato");
+    }
+
+    public Die effetto(Die dado){
+        int faccia = dado.getFace();
+        switch (faccia){
+            case 1: dado.setFace(6);
+            case 2: dado.setFace(5);
+            case 3: dado.setFace(4);
+            case 4: dado.setFace(3);
+            case 5: dado.setFace(2);
+            case 6: dado.setFace(1);
+        }
+        return dado;
     }
 
 }
