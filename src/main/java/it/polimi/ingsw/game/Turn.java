92dbbfc347b0ef5f7873f87209fca573d5df120c
demon <sara.demonaco@mail.polimi.it>
2018-05-18 11:32:27 +0200

    Compilation Error class Turn

--- src/main/java/it/polimi/ingsw/game/Turn.java
+++ src/main/java/it/polimi/ingsw/game/Turn.java
@@ -2,17 +2,7 @@ package it.polimi.ingsw.game;
 
 public class Turn {
     private Player oneplayer;
-    private TurnHandler gestoreTurno = new TurnHandler();
 
-
-
-    public Turn(Player player) {
-        this.oneplayer=player;
-    }
-
-    public Player getOneplayer() {
-        return oneplayer;
-    }
 
 
     @Override
