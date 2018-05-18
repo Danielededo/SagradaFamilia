058690ac8d732ab7b2086b2e172163fa0b7d9991
mariodiodoardo <37807412+mariodiodoardo@users.noreply.github.com>
2018-05-18 19:35:23 +0200

    update server and client connection whit observer pattern

--- src/main/java/it/polimi/ingsw/rete/Client_starter.java
+++
@@ -1,9 +0,0 @@
-package it.polimi.ingsw.rete;
-
-public class Client_starter {
-
-    /*public static void main(String[] args) {
-        Client client=new Client();
-        client.connect();
-    }*/
-}
