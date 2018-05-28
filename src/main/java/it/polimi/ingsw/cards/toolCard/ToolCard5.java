295f1689121d69d284f53b9b40b41f7204a0ccbc
Danielededo <daniele.dedominicis@mail.polimi.it>
2018-05-28 19:34:55 +0200

    add statement to some tool cards

--- src/main/java/it/polimi/ingsw/cards/toolCard/ToolCard5.java
+++ src/main/java/it/polimi/ingsw/cards/toolCard/ToolCard5.java
@@ -21,6 +21,7 @@ public class ToolCard5 extends Tool {
             if (!this.isAccessed()) {
                 if (getPlayer().getMarker() > 0) {
                     getPlayer().setMarker(getPlayer().getMarker() - 1);
+                    setAccessed(true);
                     setUsed(true);
                 } else {
                     System.out.println("Non puoi utilizzare questa carta Tool perchè non possiedi abbastanza segnalini favore");
