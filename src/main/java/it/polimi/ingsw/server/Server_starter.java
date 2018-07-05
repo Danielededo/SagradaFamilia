ff1a3640ef32a9721e0f85383081102aceaf1d37
mariodiodoardo <37807412+mariodiodoardo@users.noreply.github.com>
2018-07-05 19:31:26 +0200

    fixed timer initialization

--- src/main/java/it/polimi/ingsw/server/Server_starter.java
+++ src/main/java/it/polimi/ingsw/server/Server_starter.java
@@ -1,13 +1,35 @@
 package it.polimi.ingsw.server;
 
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Properties;
+
 public class Server_starter {
     public static void main(String[] args) {
         try {
+            int timer_window=0;
+            int timer_t=0;
+            int timer_waiting=0;
+            Properties properties=new Properties();
+            String path="src/main/resources/timer.properties";
+            try {
+                properties.load(new FileReader(path));
+                timer_window= Integer.parseInt(properties.getProperty("Timer_window"));
+                timer_t= Integer.parseInt(properties.getProperty("Timer_turn"));
+                timer_waiting= Integer.parseInt(properties.getProperty("Timer_waiting"));
+            } catch (IOException e) {}
+            if (timer_t<=0||timer_waiting<=0||timer_window<=0){
+                System.err.println("Valori di uno o più timer non idonei");
+                System.exit(-1);
+            }
             args[0]=args[0].replaceAll("-","");
             Server server= new Server(args[0]);
             server.start_server();
         }catch (ArrayIndexOutOfBoundsException e){
             System.err.println("Non è stata inserita nessuna porta");
+            System.exit(-1);
+        }catch (NumberFormatException e){
+            System.err.println("Valori di uno o più timer non idonei");
             System.exit(-1);
         }
     }
